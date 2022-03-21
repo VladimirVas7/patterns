13f92e7fb625699f7c3b2adcb9412a4d293ae64a
VladimirVas7 <90377787+VladimirVas7@users.noreply.github.com>
2022-03-21 10:55:37 +0200

    Add files via upload

---
+++ SingletonPattern-master/SingletonPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+               () => PrintStudentDetails(), 
+                () => PrintEmployeeDetails()
+                );
+            Console.ReadLine();
+        }
+
+        private static void PrintStudentDetails()
+        {
+            Singleton sobj = Singleton.Instance;
+            sobj.DisplayValues("this is from Student class");
+        }
+
+        private static void PrintEmployeeDetails()
+        {
+            Singleton eobj = Singleton.Instance;
+            eobj.DisplayValues("this is from Employee class");
+        }
+    }
+}
---
+++ SingletonPattern-master/SingletonPattern/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace SingletonPattern
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance;
+        private int counter = 0;
+        private static readonly Object obj = new object();
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter: " + counter.ToString());
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (obj)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Singleton();
+                        };
+                    }
+                }
+                return instance;
+            }
+        }
+
+        public void DisplayValues(string input)
+        {
+            Console.WriteLine(input); 
+        }
+    }
+}
