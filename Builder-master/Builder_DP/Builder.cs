027fb3b55722f4b955b30523d95139b183845df9
VladimirVas7 <90377787+VladimirVas7@users.noreply.github.com>
2022-03-21 11:03:35 +0200

    Add files via upload

---
+++ Builder-master/Builder_DP/Builder.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//builders for different bodies created inheriting the base class Builder
+namespace Builder_DP
+{
+    public abstract class ConcreteBuilder
+    {
+        public  Car Car { get; protected set; }
+        public ConcreteBuilder()
+        {
+            Car = new Car();
+        }
+        public abstract void SetBody();
+        public abstract void SetEngine();
+        public abstract void SetTires();
+        public abstract void SetEfficiency();
+    }
+    public class SedanBuilder : ConcreteBuilder
+    {
+        Sedan Sedan = new Sedan();
+        public override void SetBody()
+        {
+            Car.Body = Sedan.Body;
+        }
+
+        public override void SetEfficiency()
+        {
+            Car.Efficiency = Sedan.Efficiency;
+        }
+
+        public override void SetEngine()
+        {
+            Car.Engine = Sedan.Engine;
+        }
+
+        public override void SetTires()
+        {
+            Car.Tires = Sedan.Tires;
+        }
+    }   
+    public class CoupeBuilder:ConcreteBuilder
+    {
+        Coupe Coupe = new Coupe();
+
+        public override void SetBody()
+        {
+            Car.Body = Coupe.Body;
+        }
+
+        public override void SetEfficiency()
+        {
+            Car.Efficiency = Coupe.Efficiency;
+        }
+
+        public override void SetEngine()
+        {
+            Car.Engine = Coupe.Engine;
+        }
+
+        public override void SetTires()
+        {
+            Car.Tires = Coupe.Tires;
+        }
+    }
+    public class UniversalBuilder : ConcreteBuilder
+    {
+        Universal universal = new Universal();
+        public override void SetBody()
+        {
+            Car.Body = universal.Body;
+        }
+
+        public override void SetEfficiency()
+        {
+            Car.Efficiency = universal.Efficiency;
+        }
+
+        public override void SetEngine()
+        {
+            Car.Engine = universal.Engine;
+        }
+
+        public override void SetTires()
+        {
+            Car.Tires = universal.Tires;
+        }
+    }
+    public class HatchBackBuilder : ConcreteBuilder
+    {
+        HatchBack hatchBack = new HatchBack();
+        public override void SetBody()
+        {
+            Car.Body = hatchBack.Body;
+        }
+
+        public override void SetEfficiency()
+        {
+            Car.Efficiency = hatchBack.Efficiency;
+        }
+
+        public override void SetEngine()
+        {
+            Car.Engine = hatchBack.Engine;
+        }
+
+        public override void SetTires()
+        {
+            Car.Tires = hatchBack.Tires;
+        }
+    }
+}
---
+++ Builder-master/Builder_DP/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder_DP
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public string Body { get; set; }
+        public int Engine { get; set; }
+        public int Tires { get; set; }
+        public string Efficiency { get; set; }
+        public void Info()
+        {
+            Console.WriteLine("=============================");
+            Console.WriteLine($"Car name: {Name}");
+            Console.WriteLine($"Body: {Body}");
+            Console.WriteLine($"Engine: {Engine}hp");
+            Console.WriteLine($"Tires: {Tires}");
+            Console.WriteLine($"Efficiency: {Efficiency}");
+        }
+    }
+    public class Shop
+    {
+        public void Create(ConcreteBuilder concreteBuilder)
+        {
+            concreteBuilder.SetBody();
+            concreteBuilder.SetEngine();
+            concreteBuilder.SetTires();
+            concreteBuilder.SetEfficiency();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shop shop = new Shop();
+            SedanBuilder sedanBuilder = new SedanBuilder();
+            shop.Create(sedanBuilder);
+            Car car = sedanBuilder.Car;
+            car.Name = "Daewoo Lanos";
+            car.Info();
+
+
+            HatchBackBuilder hatchBackBuilder = new HatchBackBuilder();
+            shop.Create(hatchBackBuilder);
+            Car car2 = hatchBackBuilder.Car;
+            car2.Name = "Hyundai Getz";
+            car2.Info();
+        }
+    }
+}
